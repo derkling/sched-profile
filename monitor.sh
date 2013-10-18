02cabf7aec6163a84cac21ac6f2febcaa50c141c
Patrick Bellasi <derkling@gmail.com>
2013-10-18 17:49:49 +0200

    [Monitor] Add a set of test monitors
    This script is used to setup a set of RT monitoring tasks which could
    be useful to check the testing progresses as well as to verify their
    proper execution.
    
    Signed-off-by: Patrick Bellasi <derkling@gmail.com>

---
+++ monitor.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Ensure the monitor is running on the HOST partition
+echo $$ > cgroup/host/cgroup.procs
+chrt -f 10 xterm -geometry 139x70--9+-8 -e htop &
+chrt -f 10 xterm -geometry 139x29+-9+-8 -e 'watch -n1 "grep \"cbs_rq\[3\" -A25 /proc/sched_debug"' &
+chrt -f 10 xterm -geometry 139x29+-9+-8 -e 'watch -n1 "grep \"rt_rq\[3\" -A25 /proc/sched_debug"' &
+chrt -f 11 xterm -geometry 139x29--9-16 &
