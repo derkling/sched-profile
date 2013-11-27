835d8d9ef5274c2ba7fd891adf61b66186b61f31
Patrick Bellasi <derkling@gmail.com>
2013-11-27 14:54:01 +0100

    [Monitor] Update to show all sched_debug info
    Signed-off-by: Patrick Bellasi <derkling@gmail.com>

--- monitor.sh
+++ monitor.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+TERM=${TERM:-gnome-terminal}
+
 # Ensure the monitor is running on the HOST partition
 echo $$ > cgroup/host/cgroup.procs
-chrt -f 10 xterm -geometry 139x70--9+-8 -e htop &
-chrt -f 10 xterm -geometry 139x29+-9+-8 -e 'watch -n1 "grep \"cbs_rq\[3\" -A25 /proc/sched_debug"' &
-chrt -f 10 xterm -geometry 139x29+-9+-8 -e 'watch -n1 "grep \"rt_rq\[3\" -A25 /proc/sched_debug"' &
-chrt -f 11 xterm -geometry 139x29--9-16 &
+chrt -f 10 $TERM -geometry 139x70--9+-8 -e htop &
+chrt -f 10 $TERM -geometry 139x29+-9+-8 -e 'watch -n1 "grep \"cbs_rq\[3\" -A32 /proc/sched_debug"' &
+chrt -f 10 $TERM -geometry 139x29+-9+-8 -e 'watch -n1 "grep \"rt_rq\[3\" -A25 /proc/sched_debug"' &
+chrt -f 11 $TERM -geometry 139x29--9-16 &
